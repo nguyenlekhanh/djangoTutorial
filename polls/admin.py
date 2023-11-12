1b67617902c55dc78c5485b52d37b546060a9de0
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-11-12 17:39:25 -0600

    basic customizing the admin site

--- polls/admin.py
+++ polls/admin.py
@@ -4,6 +4,24 @@ from .models import Question, Choice
 
 
 # Register your models here.
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
+# reordering field
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ["pub_date", "question_text"]
+
+# split the form up into fieldsets:
+class QuestionAdmin(admin.ModelAdmin):
+    #Customize the admin change list
+    list_display = ["question_text", "pub_date"]
+
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
