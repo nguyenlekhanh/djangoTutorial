3422ac6e57b8dd5375badbac701988e8761b57a9
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-11-12 22:59:27 -0600

    show api route, and install django rest framework

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("polls/", include("polls.urls")),
     path('', include('base.urls')),
+    path('api/', include('base.api.urls')),
 ]
