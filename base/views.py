21136b54d87e0ee8f08e3970a3be9d80531cc6f1
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-11-12 19:54:57 -0600

    create room model

--- base/views.py
+++ base/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'First room'},
-    {'id': 2, 'name': 'Second room'},
-    {'id': 3, 'name': 'Third room'},
-    {'id': 4, 'name': 'Third room'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'First room'},
+#     {'id': 2, 'name': 'Second room'},
+#     {'id': 3, 'name': 'Third room'},
+#     {'id': 4, 'name': 'Third room'},
+# ]
 
 def home(request):
     #template = loader.get_template("base/home.html")
+    rooms = Room.objects.all();
     context = {'rooms': rooms};
     return render(request, "base/home.html", context)
 
@@ -24,7 +26,7 @@ def room(request):
 
 def room_detail(request, pk):
     #template = loader.get_template("base/room.html")
-    room = None
+    room = Room.objects.get(id=pk)
     for i in rooms:
         if i['id']== int(pk):
             room = i
