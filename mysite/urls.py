29f6d7891370072e677adf8c3fe8ee8229fb0cc9
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-11-13 11:54:08 -0600

    add some fields (name, email, avatar,...) to User model

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,6 @@ urlpatterns = [
     path('', include('base.urls')),
     path('api/', include('base.api.urls')),
 ]
+
+#set media_url get image from media_root
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
